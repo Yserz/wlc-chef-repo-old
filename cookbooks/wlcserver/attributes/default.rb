31879ae9295ac685083ba2f2575c06352e9add26
Yserz <michael.koppen@googlemail.com>
2013-08-26 20:54:00 +0200

    added jdbc resources

--- cookbooks/wlcserver/attributes/default.rb
+++ cookbooks/wlcserver/attributes/default.rb
@@ -6,3 +6,50 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+default['wlcserver']['glassfish']['domain_name'] = "domain1"
+default['wlcserver']['glassfish']['admin_name'] = "admin"
+default['wlcserver']['glassfish']['admin_port'] = 4848
+default['wlcserver']['glassfish']['admin_password'] = "password"
+default['wlcserver']['glassfish']['terse'] = false
+default['wlcserver']['glassfish']['echo'] = true
+default['wlcserver']['glassfish']['secure'] = false
+
+default['wlcserver']['glassfish']['jdbc_connection_pool']['name'] = "CodingConnectionPool"
+default['wlcserver']['glassfish']['jdbc_connection_pool']['description'] = ""
+default['wlcserver']['glassfish']['jdbc_connection_pool']['allow-non-component-callers'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['associate-with-thread'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['connection-creation-retry-attempts'] = 0
+default['wlcserver']['glassfish']['jdbc_connection_pool']['connection-creation-retry-interval-in-seconds'] = 10
+default['wlcserver']['glassfish']['jdbc_connection_pool']['connection-leak-reclaim'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['connection-leak-timeout-in-seconds'] = 0
+default['wlcserver']['glassfish']['jdbc_connection_pool']['connection-validation-method'] = "auto-commit"
+default['wlcserver']['glassfish']['jdbc_connection_pool']['datasource-classname'] = "com.mysql.jdbc.jdbc2.optional.MysqlDataSource"
+default['wlcserver']['glassfish']['jdbc_connection_pool']['fail-all-connections'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['idle-timeout-in-seconds'] = 300
+default['wlcserver']['glassfish']['jdbc_connection_pool']['is-connection-validation-required'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['is-isolation-level-guaranteed'] = true
+default['wlcserver']['glassfish']['jdbc_connection_pool']['lazy-connection-association'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['lazy-connection-enlistment'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['match-connections'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['max-connection-usage-count'] = 0
+default['wlcserver']['glassfish']['jdbc_connection_pool']['max-pool-size'] = 32
+default['wlcserver']['glassfish']['jdbc_connection_pool']['max-wait-time-in-millis'] = 60000
+default['wlcserver']['glassfish']['jdbc_connection_pool']['non-transactional-connections'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['ping'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['pool-resize-quantity'] = 2
+default['wlcserver']['glassfish']['jdbc_connection_pool']['pooling'] = true
+default['wlcserver']['glassfish']['jdbc_connection_pool']['res-type'] = "javax.sql.DataSource"
+default['wlcserver']['glassfish']['jdbc_connection_pool']['statement-cache-size'] = 0
+default['wlcserver']['glassfish']['jdbc_connection_pool']['statement-leak-reclaim'] = false
+default['wlcserver']['glassfish']['jdbc_connection_pool']['statement-leak-timeout-in-seconds'] = 0
+default['wlcserver']['glassfish']['jdbc_connection_pool']['statement-timeout-in-seconds'] = -1
+default['wlcserver']['glassfish']['jdbc_connection_pool']['steady-pool-size'] = 8
+default['wlcserver']['glassfish']['jdbc_connection_pool']['validate-atmost-once-period-in-seconds'] = 0
+default['wlcserver']['glassfish']['jdbc_connection_pool']['wrap-jdbc-objects'] = false
+
+
+default['wlcserver']['glassfish']['jdbc_resource']['pool-name'] = node['wlcserver']['glassfish']['jdbc_connection_pool']['name']
+default['wlcserver']['glassfish']['jdbc_resource']['jndi-name'] = "jdbc/coding"
+default['wlcserver']['glassfish']['jdbc_resource']['enabled'] = true
+default['wlcserver']['glassfish']['jdbc_resource']['description'] = ""
+
--- cookbooks/wlcserver/recipes/default.rb
+++ cookbooks/wlcserver/recipes/default.rb
@@ -18,56 +18,80 @@ include_recipe "jenkins"
 #Create Production-Database for WLC
 mysql_database node['wlcserver']['database'] do
   connection ({
-    :host => 'localhost', 
-    :username => 'root', 
-    :password => node['mysql']['server_root_password']})
-  action :create
+                                :host => 'localhost', 
+                                :username => 'root', 
+                                :password => node['mysql']['server_root_password']})
+  action                      :create
 end
 
 # Create Test-Database for WLC
 mysql_database node['wlcserver']['test_database'] do
   connection ({
-    :host => 'localhost',
-    :username => 'root',
-    :password => node['mysql']['server_root_password']})
-  action :create
+                                :host => 'localhost',
+                                :username => 'root',
+                                :password => node['mysql']['server_root_password']})
+  action                      :create
 end
 
 # Create Database-User and grant access for Production-Database
-mysql_database_user node['wlcserver']['db_username'] do
+mysql_database_user node['wlcserver']['database_username'] do
   connection ({
-    :host => 'localhost', 
-    :username => 'root', 
-    :password => node['mysql']['server_root_password']})
-  password node['wlcserver']['database_password']
-  database_name node['wlcserver']['database']
-  privileges [:select,:update,:insert,:create,:delete]
-  action :grant
+                                :host => 'localhost', 
+                                :username => 'root', 
+                                :password => node['mysql']['server_root_password']})
+  password                    node['wlcserver']['database_password']
+  database_name               node['wlcserver']['database']
+  privileges                  [:select,:update,:insert,:create,:delete]
+  action                      :grant
 end
 
 # Create Database-User and grant access for Test-Database
-mysql_database_user node['wlcserver']['db_username'] do
+mysql_database_user node['wlcserver']['database_username'] do
   connection ({
-    :host => 'localhost',
-    :username => 'root',
-    :password => node['mysql']['server_root_password']})
-  password node['wlcserver']['database_password']
-  database_name node['wlcserver']['test_database']
-  privileges [:select,:update,:insert,:create,:delete]
-  action :grant
+                                :host => 'localhost',
+                                :username => 'root',
+                                :password => node['mysql']['server_root_password']})
+  password                    node['wlcserver']['database_password']
+  database_name               node['wlcserver']['test_database']
+  privileges                  [:select,:update,:insert,:create,:delete]
+  action                      :grant
 end
 
 # GLASSFISH #
+# Create Domain
+glassfish_domain "domain1" do
+  domain_name                 node['wlcserver']['glassfish']['domain_name']
+  port                        80
+  username                    node['wlcserver']['glassfish']['admin_name']
+  password_file               node['wlcserver']['glassfish']['admin_password']
+  admin_port                  node['wlcserver']['glassfish']['admin_port']
+  secure                      node['wlcserver']['glassfish']['secure']
+  terse                       node['wlcserver']['glassfish']['terse']
+  echo                        node['wlcserver']['glassfish']['echo']
+  extra_libraries             ['https://github.com/Yserz/WLC-Chef-Repo/blob/master/mysql-connector-java-5.1.26-bin.jar']
+  max_stack_size              512
+end
+
 # Create WLC JDBC Connection Pool
 glassfish_jdbc_connection_pool node['wlcserver']['glassfish']['jdbc_connection_pool']['name'] do
-  domain_name node['wlcserver']['glassfish']['domain_name']
-  terse false
-  echo: true
-  username: The username to use when communicating with the domain. Defaults to nil.
-  password_file: The file in which the password must be stored assigned to appropriate key. Defaults to nil.
-  secure: If true use SSL when communicating with the domain for administration. Defaults to false.
-  admin_port: The port on which the web management console is bound. Defaults to 4848.
+  # Global settings
+  domain_name                 node['wlcserver']['glassfish']['domain_name']
+  username                    node['wlcserver']['glassfish']['admin_name']
+  password_file               node['wlcserver']['glassfish']['admin_password']
+  admin_port                  node['wlcserver']['glassfish']['admin_port']
+  secure                      node['wlcserver']['glassfish']['secure']
+  terse                       node['wlcserver']['glassfish']['terse']
+  echo                        node['wlcserver']['glassfish']['echo']
+  
+  # Pool settings
+  #  initsql nil
+  #  sqltracelisteners nil
+  #  driverclassname node['wlcserver']['glassfish']['jdbc_connection_pool']['']
+  #  validationclassname nil
+  #  validationtable nil
+  #  isolationlevel:
   pool_name                   node['wlcserver']['glassfish']['jdbc_connection_pool']['name']
+  description                 node['wlcserver']['glassfish']['jdbc_connection_pool']['description']
   allownoncomponentcallers    node['wlcserver']['glassfish']['jdbc_connection_pool']['allow-non-component-callers']
   associatewiththread         node['wlcserver']['glassfish']['jdbc_connection_pool']['associate-with-thread']
   creationretryattempts       node['wlcserver']['glassfish']['jdbc_connection_pool']['connection-creation-retry-attempts']
@@ -98,27 +122,42 @@ glassfish_jdbc_connection_pool node['wlcserver']['glassfish']['jdbc_connection_p
   steadypoolsize              node['wlcserver']['glassfish']['jdbc_connection_pool']['steady-pool-size']
   validateatmostonceperiod    node['wlcserver']['glassfish']['jdbc_connection_pool']['validate-atmost-once-period-in-seconds']
   wrapjdbcobjects             node['wlcserver']['glassfish']['jdbc_connection_pool']['wrap-jdbc-objects']
-x  properties: Defaults to {}.
+  properties {
+    "URL":                      "jdbc:mysql://localhost:3306/#{node['wlcserver']['database']}",
+    "User":                     node['wlcserver']['database_username'],
+    "Password":                 node['wlcserver']['database_password']
+  }
+end
 
-#  initsql nil
-#  sqltracelisteners nil
-#  driverclassname node['wlcserver']['glassfish']['jdbc_connection_pool']['']
-#  validationclassname nil
-#  validationtable nil
-#  isolationlevel:
+glassfish_jdbc_resource node['wlcserver']['glassfish']['jdbc_resource']['name'] do
+  # Global settings
+  domain_name                 node['wlcserver']['glassfish']['domain_name']
+  username                    node['wlcserver']['glassfish']['admin_name']
+  password_file               node['wlcserver']['glassfish']['admin_password']
+  admin_port                  node['wlcserver']['glassfish']['admin_port']
+  secure                      node['wlcserver']['glassfish']['secure']
+  terse                       node['wlcserver']['glassfish']['terse']
+  echo                        node['wlcserver']['glassfish']['echo']
+  
+  # Resource settings
+  connectionpoolid            node['wlcserver']['glassfish']['jdbc_resource']['name']
+  resource_name               node['wlcserver']['glassfish']['jdbc_resource']['name']
+  enabled                     node['wlcserver']['glassfish']['jdbc_resource']['enabled']
+  description                 node['wlcserver']['glassfish']['jdbc_resource']['description']
+#  target                      
+#  properties               
+end
 
-
-
-  realm_name:
-  target: Defaults to "server".
-  classname:
-  jaas_context node['wlcserver']['glassfish']['jdbc_connection_pool']['']
-  assign_groups node['wlcserver']['glassfish']['jdbc_connection_pool']['']
-  properties: Defaults to {}.
-  domain_name: The name of the domain.
-  terse: Use terse output from the underlying asadmin. Defaults to false.
-  echo: If true, echo commands supplied to asadmin. Defaults to true.
-  username: The username to use when communicating with the domain. Defaults to nil.
-  password_file: The file in which the password must be stored assigned to appropriate key. Defaults to nil.
-  secure: If true use SSL when communicating with the domain for administration. Defaults to false.
-  admin_port: The port on which the web management console is bound. Defaults to 4848.
+#  realm_name:
+#  target: Defaults to "server".
+#  classname:
+#  jaas_context node['wlcserver']['glassfish']['jdbc_connection_pool']['']
+#  assign_groups node['wlcserver']['glassfish']['jdbc_connection_pool']['']
+#  properties: Defaults to {}.
+#  domain_name: The name of the domain.
+#  terse: Use terse output from the underlying asadmin. Defaults to false.
+#  echo: If true, echo commands supplied to asadmin. Defaults to true.
+#  username: The username to use when communicating with the domain. Defaults to nil.
+#  password_file: The file in which the password must be stored assigned to appropriate key. Defaults to nil.
+#  secure: If true use SSL when communicating with the domain for administration. Defaults to false.
+#  admin_port: The port on which the web management console is bound. Defaults to 4848.
