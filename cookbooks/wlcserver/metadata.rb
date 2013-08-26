6469af4bc4891cd2f8249aa01fe1ee21f2ee61d7
root <root@localhost.localdomain>
2013-08-26 16:13:02 +0200

    init

---
+++ cookbooks/wlcserver/metadata.rb
@@ -0,0 +1,9 @@
+name             'wlcserver'
+maintainer       'Michael Koppen'
+maintainer_email 'Michael.Koppen@googlemail.com'
+license          'All rights reserved'
+description      'Installs/Configures wlcserver'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "apt"
