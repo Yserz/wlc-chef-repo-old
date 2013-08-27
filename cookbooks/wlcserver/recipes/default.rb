4e88dcc5ccda8b6665b1e7c70a93aa0141f3104a
Yserz <michael.koppen@googlemail.com>
2013-08-27 15:04:51 +0200

    added test resources

--- cookbooks/wlcserver/recipes/default.rb
+++ cookbooks/wlcserver/recipes/default.rb
@@ -74,6 +74,7 @@ glassfish_domain "domain1" do
   max_stack_size              512
 end
 
+# Create AuthRealm
 glassfish_secure_admin node['wlcserver']['glassfish']['domain_name'] do
   # Global settings
   domain_name                 node['wlcserver']['glassfish']['domain_name']
@@ -87,6 +88,7 @@ glassfish_secure_admin node['wlcserver']['glassfish']['domain_name'] do
   action :enable
 end
 
+# Add MySQL JDBC-Driver
 glassfish_library 'http://daisy.trac.cvsdude.com/pipeline/export/15/persontest/trunk/db/mysql/mysql-connector-java-5.1.6-bin.jar' do
   domain_name                 node['wlcserver']['glassfish']['domain_name']
   admin_port                  node['wlcserver']['glassfish']['admin_port']
@@ -97,8 +99,8 @@ glassfish_library 'http://daisy.trac.cvsdude.com/pipeline/export/15/persontest/t
   upload                      false
 end
 
-# Create WLC JDBC Connection Pool
-glassfish_jdbc_connection_pool node['wlcserver']['glassfish']['jdbc_connection_pool']['name'] do
+# Create WLC JDBC PRODUCTION CONNECTION POOL
+glassfish_jdbc_connection_pool node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['name'] do
   # Global settings
   domain_name                 node['wlcserver']['glassfish']['domain_name']
   username                    node['wlcserver']['glassfish']['admin_name']
@@ -111,50 +113,51 @@ glassfish_jdbc_connection_pool node['wlcserver']['glassfish']['jdbc_connection_p
   # Pool settings
   #  initsql nil
   #  sqltracelisteners nil
-  #  driverclassname node['wlcserver']['glassfish']['jdbc_connection_pool']['']
+  #  driverclassname node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['']
   #  validationclassname nil
   #  validationtable nil
   #  isolationlevel:
-  pool_name                   node['wlcserver']['glassfish']['jdbc_connection_pool']['name']
-  description                 node['wlcserver']['glassfish']['jdbc_connection_pool']['description']
-  allownoncomponentcallers    node['wlcserver']['glassfish']['jdbc_connection_pool']['allow-non-component-callers']
-  associatewiththread         node['wlcserver']['glassfish']['jdbc_connection_pool']['associate-with-thread']
-  creationretryattempts       node['wlcserver']['glassfish']['jdbc_connection_pool']['connection-creation-retry-attempts']
-  creationretryinterval       node['wlcserver']['glassfish']['jdbc_connection_pool']['connection-creation-retry-interval-in-seconds']
-  leakreclaim                 node['wlcserver']['glassfish']['jdbc_connection_pool']['connection-leak-reclaim']
-  leaktimeout                 node['wlcserver']['glassfish']['jdbc_connection_pool']['connection-leak-timeout-in-seconds']
-  validationmethod            node['wlcserver']['glassfish']['jdbc_connection_pool']['connection-validation-method']
-  datasourceclassname         node['wlcserver']['glassfish']['jdbc_connection_pool']['datasource-classname']
-  failconnection              node['wlcserver']['glassfish']['jdbc_connection_pool']['fail-all-connections']
-  idletimeout                 node['wlcserver']['glassfish']['jdbc_connection_pool']['idle-timeout-in-seconds']
-  isconnectvalidatereq        node['wlcserver']['glassfish']['jdbc_connection_pool']['is-connection-validation-required']
-  isisolationguaranteed       node['wlcserver']['glassfish']['jdbc_connection_pool']['is-isolation-level-guaranteed']
-  lazyconnectionassociation   node['wlcserver']['glassfish']['jdbc_connection_pool']['lazy-connection-association']
-  lazyconnectionenlistment    node['wlcserver']['glassfish']['jdbc_connection_pool']['lazy-connection-enlistment']
-  matchconnections            node['wlcserver']['glassfish']['jdbc_connection_pool']['match-connections']
-  maxconnectionusagecount     node['wlcserver']['glassfish']['jdbc_connection_pool']['max-connection-usage-count']
-  maxpoolsize                 node['wlcserver']['glassfish']['jdbc_connection_pool']['max-pool-size']
-  maxwait                     node['wlcserver']['glassfish']['jdbc_connection_pool']['max-wait-time-in-millis']
-  nontransactionalconnections node['wlcserver']['glassfish']['jdbc_connection_pool']['non-transactional-connections']
-  ping                        node['wlcserver']['glassfish']['jdbc_connection_pool']['ping']
-  poolresize                  node['wlcserver']['glassfish']['jdbc_connection_pool']['pool-resize-quantity']
-  pooling                     node['wlcserver']['glassfish']['jdbc_connection_pool']['pooling']
-  restype                     node['wlcserver']['glassfish']['jdbc_connection_pool']['res-type']
-  statementcachesize          node['wlcserver']['glassfish']['jdbc_connection_pool']['statement-cache-size']
-  statmentleakreclaim         node['wlcserver']['glassfish']['jdbc_connection_pool']['statement-leak-reclaim']
-  statementleaktimeout        node['wlcserver']['glassfish']['jdbc_connection_pool']['statement-leak-timeout-in-seconds']
-  statementtimeout            node['wlcserver']['glassfish']['jdbc_connection_pool']['statement-timeout-in-seconds']
-  steadypoolsize              node['wlcserver']['glassfish']['jdbc_connection_pool']['steady-pool-size']
-  validateatmostonceperiod    node['wlcserver']['glassfish']['jdbc_connection_pool']['validate-atmost-once-period-in-seconds']
-  wrapjdbcobjects             node['wlcserver']['glassfish']['jdbc_connection_pool']['wrap-jdbc-objects']
+  pool_name                   node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['name']
+  description                 node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['description']
+  allownoncomponentcallers    node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['allow-non-component-callers']
+  associatewiththread         node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['associate-with-thread']
+  creationretryattempts       node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['connection-creation-retry-attempts']
+  creationretryinterval       node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['connection-creation-retry-interval-in-seconds']
+  leakreclaim                 node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['connection-leak-reclaim']
+  leaktimeout                 node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['connection-leak-timeout-in-seconds']
+  validationmethod            node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['connection-validation-method']
+  datasourceclassname         node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['datasource-classname']
+  failconnection              node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['fail-all-connections']
+  idletimeout                 node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['idle-timeout-in-seconds']
+  isconnectvalidatereq        node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['is-connection-validation-required']
+  isisolationguaranteed       node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['is-isolation-level-guaranteed']
+  lazyconnectionassociation   node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['lazy-connection-association']
+  lazyconnectionenlistment    node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['lazy-connection-enlistment']
+  matchconnections            node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['match-connections']
+  maxconnectionusagecount     node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['max-connection-usage-count']
+  maxpoolsize                 node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['max-pool-size']
+  maxwait                     node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['max-wait-time-in-millis']
+  nontransactionalconnections node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['non-transactional-connections']
+  ping                        node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['ping']
+  poolresize                  node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['pool-resize-quantity']
+  pooling                     node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['pooling']
+  restype                     node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['res-type']
+  statementcachesize          node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['statement-cache-size']
+  statmentleakreclaim         node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['statement-leak-reclaim']
+  statementleaktimeout        node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['statement-leak-timeout-in-seconds']
+  statementtimeout            node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['statement-timeout-in-seconds']
+  steadypoolsize              node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['steady-pool-size']
+  validateatmostonceperiod    node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['validate-atmost-once-period-in-seconds']
+  wrapjdbcobjects             node['wlcserver']['glassfish']['production']['jdbc_connection_pool']['wrap-jdbc-objects']
   properties ({
-    :URL =>                      "jdbc:mysql://localhost:3306/#{node['wlcserver']['database']}",
+    :URL =>                      "jdbc:mysql://#{node['mysql']['bind_address']}:#{node['mysql']['port']}/#{node['wlcserver']['database']}",
     :User =>                     node['wlcserver']['database_username'],
     :Password =>                 node['wlcserver']['database_password']
   })
 end
 
-glassfish_jdbc_resource node['wlcserver']['glassfish']['jdbc_resource']['jndi-name'] do
+# Create JDBC PRODUCTION RESOURCE
+glassfish_jdbc_resource node['wlcserver']['glassfish']['production']['jdbc_resource']['jndi-name'] do
   # Global settings
   domain_name                 node['wlcserver']['glassfish']['domain_name']
   username                    node['wlcserver']['glassfish']['admin_name']
@@ -165,10 +168,88 @@ glassfish_jdbc_resource node['wlcserver']['glassfish']['jdbc_resource']['jndi-na
   echo                        node['wlcserver']['glassfish']['echo']
   
   # Resource settings
-  connectionpoolid            node['wlcserver']['glassfish']['jdbc_resource']['pool-name']
-  resource_name               node['wlcserver']['glassfish']['jdbc_resource']['jndi-name']
-  enabled                     node['wlcserver']['glassfish']['jdbc_resource']['enabled']
-  description                 node['wlcserver']['glassfish']['jdbc_resource']['description']
+  connectionpoolid            node['wlcserver']['glassfish']['production']['jdbc_resource']['pool-name']
+  resource_name               node['wlcserver']['glassfish']['production']['jdbc_resource']['jndi-name']
+  enabled                     node['wlcserver']['glassfish']['production']['jdbc_resource']['enabled']
+  description                 node['wlcserver']['glassfish']['production']['jdbc_resource']['description']
+#  target                      
+#  properties               
+end
+
+
+# Create WLC JDBC TEST CONNECTION POOL
+glassfish_jdbc_connection_pool node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['name'] do
+  # Global settings
+  domain_name                 node['wlcserver']['glassfish']['domain_name']
+  username                    node['wlcserver']['glassfish']['admin_name']
+  password_file               password_file
+  admin_port                  node['wlcserver']['glassfish']['admin_port']
+  secure                      node['wlcserver']['glassfish']['secure']
+  terse                       node['wlcserver']['glassfish']['terse']
+  echo                        node['wlcserver']['glassfish']['echo']
+  
+  # Pool settings
+  #  initsql nil
+  #  sqltracelisteners nil
+  #  driverclassname node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['']
+  #  validationclassname nil
+  #  validationtable nil
+  #  isolationlevel:
+  pool_name                   node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['name']
+  description                 node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['description']
+  allownoncomponentcallers    node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['allow-non-component-callers']
+  associatewiththread         node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['associate-with-thread']
+  creationretryattempts       node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['connection-creation-retry-attempts']
+  creationretryinterval       node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['connection-creation-retry-interval-in-seconds']
+  leakreclaim                 node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['connection-leak-reclaim']
+  leaktimeout                 node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['connection-leak-timeout-in-seconds']
+  validationmethod            node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['connection-validation-method']
+  datasourceclassname         node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['datasource-classname']
+  failconnection              node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['fail-all-connections']
+  idletimeout                 node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['idle-timeout-in-seconds']
+  isconnectvalidatereq        node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['is-connection-validation-required']
+  isisolationguaranteed       node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['is-isolation-level-guaranteed']
+  lazyconnectionassociation   node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['lazy-connection-association']
+  lazyconnectionenlistment    node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['lazy-connection-enlistment']
+  matchconnections            node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['match-connections']
+  maxconnectionusagecount     node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['max-connection-usage-count']
+  maxpoolsize                 node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['max-pool-size']
+  maxwait                     node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['max-wait-time-in-millis']
+  nontransactionalconnections node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['non-transactional-connections']
+  ping                        node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['ping']
+  poolresize                  node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['pool-resize-quantity']
+  pooling                     node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['pooling']
+  restype                     node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['res-type']
+  statementcachesize          node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['statement-cache-size']
+  statmentleakreclaim         node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['statement-leak-reclaim']
+  statementleaktimeout        node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['statement-leak-timeout-in-seconds']
+  statementtimeout            node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['statement-timeout-in-seconds']
+  steadypoolsize              node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['steady-pool-size']
+  validateatmostonceperiod    node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['validate-atmost-once-period-in-seconds']
+  wrapjdbcobjects             node['wlcserver']['glassfish']['test']['jdbc_connection_pool']['wrap-jdbc-objects']
+  properties ({
+    :URL =>                      "jdbc:mysql://#{node['mysql']['bind_address']}:#{node['mysql']['port']}/#{node['wlcserver']['test_database']}",
+    :User =>                     node['wlcserver']['database_username'],
+    :Password =>                 node['wlcserver']['database_password']
+  })
+end
+
+# Create JDBC TEST RESOURCE
+glassfish_jdbc_resource node['wlcserver']['glassfish']['test']['jdbc_resource']['jndi-name'] do
+  # Global settings
+  domain_name                 node['wlcserver']['glassfish']['domain_name']
+  username                    node['wlcserver']['glassfish']['admin_name']
+  password_file               password_file
+  admin_port                  node['wlcserver']['glassfish']['admin_port']
+  secure                      node['wlcserver']['glassfish']['secure']
+  terse                       node['wlcserver']['glassfish']['terse']
+  echo                        node['wlcserver']['glassfish']['echo']
+  
+  # Resource settings
+  connectionpoolid            node['wlcserver']['glassfish']['test']['jdbc_resource']['pool-name']
+  resource_name               node['wlcserver']['glassfish']['test']['jdbc_resource']['jndi-name']
+  enabled                     node['wlcserver']['glassfish']['test']['jdbc_resource']['enabled']
+  description                 node['wlcserver']['glassfish']['test']['jdbc_resource']['description']
 #  target                      
 #  properties               
 end
